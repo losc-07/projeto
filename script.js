e76f4b520cad7159d26dd3b542a5a11713461a46
Luis O <luisotaviosousacarvalho@gmail.com>
2023-06-30 17:14:41 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function mudar_raca(){
+    let agateemeele = document.documentElement
+    agateemeele.classList.toggle('light')
+
+    let ismaige = document.querySelector("#profile img");
+
+    if (agateemeele.classList.contains('light')) {
+        ismaige.setAttribute("src", "./assets/assets/avatar-light.png")
+    } else {
+        ismaige.setAttribute("src", "./assets/assets/avatar.png")
+    }
+}
